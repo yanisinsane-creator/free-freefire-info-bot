d57a92c9c7e11dac672a3ac917814de81d85201f
Paul Alfredo <155785978+paulafredo@users.noreply.github.com>
2025-08-08 10:25:49 +0200

    Update url info and profile

--- cogs/infoCommands.py
+++ cogs/infoCommands.py
@@ -17,8 +17,8 @@ CONFIG_FILE = "info_channels.json"
 class InfoCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.api_url = "https://glob-info2.vercel.app/info"
-        self.generate_url = "https://genprofile2.vercel.app/generate"
+        self.api_url = "https://rawthug.onrender.com/info"
+        self.generate_url = "https://generatethug.onrender.com/generate"
         self.session = aiohttp.ClientSession()
         self.config_data = self.load_config()
         self.cooldowns = {}
